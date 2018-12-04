f9dd441c4d6660b59b315cdfbee924bda8194f0e
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-04 16:03:09 -0200

    Enviando repositórios com props - E1
    E1 -> Aparentemente PropTypes não funciona

--- app/src/components/CompareList/index.js
+++ app/src/components/CompareList/index.js
@@ -1,35 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
-const CompareList = () => (
+const CompareList = ({ repositories, teste }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019
-          <small>stars</small>
-        </li>
-        <li>
-          95,019
-          <small>stars</small>
-        </li>
-        <li>
-          95,019
-          <small>stars</small>
-        </li>
-        <li>
-          3 days ago
-          <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt="facebook" />
+          <strong>{teste}</strong>
+          <small>facebook</small>
+        </header>
+        <ul>
+          <li>
+            95,019
+            <small>stars</small>
+          </li>
+          <li>
+            95,019
+            <small>stars</small>
+          </li>
+          <li>
+            95,019
+            <small>stars</small>
+          </li>
+          <li>
+            3 days ago
+            <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
+
+CompareList.propTypes = {
+  teste: PropTypes.string.isRequired,
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default CompareList;
